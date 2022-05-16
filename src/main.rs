0029a07e131b4140c199982f391c17035288aaaa
home <adhyanpatel@gmail.com>
2022-05-15 20:16:50 -0600

    can read JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    let path = "subscriptions.json";
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
+    println!("{}",res)
 }
